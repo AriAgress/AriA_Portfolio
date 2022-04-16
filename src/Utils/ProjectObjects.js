a45207897d65c1e568125cde0821ea86d670ddc2
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-15 17:11:25 -0700

    Add(Utils): Objects filled out in utils folder

---
+++ src/Utils/ProjectObjects.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    projectTitle: 'Yemen-Grace Coffee',
+    description: 'lorem x3',
+    language: 'javascript',
+  },
+  {
+    projectTitle: 'East Bay Records',
+    description: 'lorem x3',
+    language: 'javascript',
+  },
+  {
+    projectTitle: 'Notepad App',
+    description: 'lorem x3',
+    language: 'javascript',
+  },
+];
