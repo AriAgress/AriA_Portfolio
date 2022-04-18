011c4a87f79ba2747694e2eb10d2ced789bbe516
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-18 15:29:53 -0700

    Add(Nav): Styled components to Nav bar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import styledComponents from 'styled-components';
+import styled from 'styled-components';
 
 const NavContainer = styled.div`
+  height: 10vh;
+
+  color: white;
+  border-bottom: 5px solid #ecb365;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const NavBody = styled.div`
+  width: 100%;
   max-width: 960px;
+
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+
+  margin-top: 6vh;
+  margin-bottom: 1vh;
+`;
+
+const LogoContainer = styled.div``;
+
+const NavLinksContainer = styled.div`
+  display: flex;
+  flex-flow: row;
+`;
+
+const Links = styled.div`
+  padding-left: 0.5vh;
+  padding-right: 0.5vh;
 `;
 
 const Nav = () => {
   return (
     <NavContainer>
-      <div>Logo</div>
-      <div>Menu</div>
-      <div>About</div>
-      <button></button>
+      <NavBody>
+        <LogoContainer>Logo</LogoContainer>
+        <NavLinksContainer>
+          <Links>Menu</Links>
+          <Links>About</Links>
+          <button>Button</button>
+        </NavLinksContainer>
+      </NavBody>
     </NavContainer>
   );
 };
