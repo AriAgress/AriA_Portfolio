e60c49e01be146b9110249a514ee0799b24409b7
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-18 15:52:54 -0700

    Add(Body): styled components to body

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const BodyWrapper = styled.div``;
-const BodyContainer = styled.div``;
-const ImgContainer = styled.div``;
-const TextContainer = styled.div``;
+const BodyWrapper = styled.div`
+  height: 60vh;
+
+  color: white;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const BodyContainer = styled.div`
+  width: 100%;
+  max-width: 960px;
+
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+
+  margin-top: 10vh;
+`;
+const ImgContainer = styled.div`
+  width: 40%;
+  background-image: url(https://picsum.photos/200/300);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+const TextContainer = styled.div`
+  width: 60%;
+
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 const DescriptionBox = styled.div``;
 
 const Body = () => {
