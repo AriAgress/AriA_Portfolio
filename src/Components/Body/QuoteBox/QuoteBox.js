d9dbdcf3dd8b43032e648e3f8e191a85a407fc5a
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-19 16:17:05 -0700

    Add(QuoteBox): styled but not finsihed

---
+++ src/Components/Body/QuoteBox/QuoteBox.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DialogBox = styled.div`
+  height: 80%;
+  width: 80%;
+  background-color: #064663;
+  border-radius: 10%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// const LeftPoint = styled.div`
+//   width: 0;
+//   height: 0;
+//   border-left: 2vh solid transparent;
+//   border-right: 2vh solid transparent;
+//   border-top: 10vh solid #064663;
+//   transform: rotate(60deg);
+
+//   position: absolute;
+// `;
+
+const TextBox = styled.div`
+  padding: 15px;
+  font-size: 45px;
+`;
+
+const QuoteBox = () => {
+  return (
+    <DialogBox>
+      <TextBox>HOW'S IT GOING? I'M ARI AGRESS.</TextBox>
+      {/* <LeftPoint /> */}
+    </DialogBox>
+  );
+};
+
+export default QuoteBox;
