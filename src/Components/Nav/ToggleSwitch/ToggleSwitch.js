76240907a52b5b01a9448dac89f5a33ffb879547
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-27 11:20:44 -0700

    Edit(ToggleSwitch): Finished colorSwitch

--- src/Components/Nav/ToggleSwitch/ToggleSwitch.js
+++ src/Components/Nav/ToggleSwitch/ToggleSwitch.js
@@ -7,23 +7,25 @@ const SwitchContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  background-color: ${props =>
-    props.colorSwitch === true ? 'rgb(210,210,210)' : 'rgb(60, 64, 67)'};
+  /* background-color: ${props =>
+    props.colorSwitch === true ? '#AEAEAE' : '#AEAEAE'}; */
+  background-color: #aeaeae;
   width: 40px;
-  height: 20px;
+  height: 15px;
 
   border-radius: 50px;
+
+  box-shadow: inset 20px 20px 60px #b3b3b3, inset -20px -20px 60px #ffffff;
 `;
 
 const Switch = styled.div`
-  background: ${props =>
-    props.colorSwitch === true ? 'rgb(32,33,35)' : 'rgb(183, 198, 185)'};
+  background: ${props => (props.colorSwitch === true ? '#ecb365' : '#064663')};
   border-radius: 50%;
-  width: 25px;
-  height: 25px;
+  width: 23px;
+  height: 23px;
   cursor: pointer;
 
-  margin-left: ${props => (props.colorSwitch === true ? '0rem' : '1rem')};
+  margin-left: ${props => (props.colorSwitch === true ? '0px' : '17px')};
 
   transition-property: margin;
   transition-duration: 0.4s;
@@ -33,7 +35,7 @@ const ToggleSwitch = () => {
   const colorSwitch = useSelector(state => state.colorSwitch);
   const dispatch = useDispatch();
   return (
-    <SwitchContainer>
+    <SwitchContainer colorSwitch={colorSwitch}>
       <Switch
         colorSwitch={colorSwitch}
         onClick={() => dispatch(setColor(!colorSwitch))}
