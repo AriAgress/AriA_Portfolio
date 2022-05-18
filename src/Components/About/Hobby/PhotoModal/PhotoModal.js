78ec5028b5ea4c521f3d4da5d5dab365f25470bb
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-05-17 17:50:47 -0700

    Add(PhotoModal): styled components and styled

--- src/Components/About/Hobby/PhotoModal/PhotoModal.js
+++ src/Components/About/Hobby/PhotoModal/PhotoModal.js
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  top: 0;
+  left: 0;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: black;
+  color: #000;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+const ModalIMG = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: white;
+`;
 
 const PhotoModal = () => {
-  return <div>Modal</div>;
+  return (
+    <Background>
+      <ModalWrapper>
+        <ModalIMG>YO-DAL</ModalIMG>
+      </ModalWrapper>
+    </Background>
+  );
 };
 
 export default PhotoModal;
