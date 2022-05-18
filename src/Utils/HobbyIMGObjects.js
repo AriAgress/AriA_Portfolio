377b9d391857129a65575ab26f9e91ef329ae9dc
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-05-18 15:54:39 -0700

    Add(HobbyIMGObjects): Objects created and img added

---
+++ src/Utils/HobbyIMGObjects.js
@@ -0,0 +1,18 @@
+import MinPhotoFriends from "../Assets/MinPhotoFriends.png";
+import StandingMan from "../Assets/SandingMan.png";
+import HappyBark from "../Assets/HappyBark.png";
+
+export const HobbyImages = [
+  {
+    projectID: 1,
+    projectImage: HappyBark,
+  },
+  {
+    projectID: 2,
+    projectImage: MinPhotoFriends,
+  },
+  {
+    projectID: 3,
+    projectImage: StandingMan,
+  },
+];
