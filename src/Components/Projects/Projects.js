b60b7b79b1157edab97a2caa2a3d716f123399f4
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-05-24 16:36:04 -0700

    Add(Projects): Modal to projects img

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { projects } from "../../Utils/ProjectObjects";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setModal } from "../../Actions/Index";
+import { setModalContent } from "../../Actions/Index";
 import Aos from "aos";
 import "aos/dist/aos.css";
+//components
+import PhotoModal from "../../Components/About/Hobby/PhotoModal/PhotoModal";
 
 // stylesheet
 import {
@@ -23,29 +27,46 @@ const Projects = () => {
   }, []);
 
   const colorSwitch = useSelector(state => state.colorSwitch);
+  const toggleModal = useSelector(state => state.toggleModal);
+  const modalContent = useSelector(state => state.modalContent);
+  const dispatch = useDispatch();
   return (
-    <ProjectsWrapper id="ToProject" data-aos="fade-right">
-      <IntroText colorSwitch={colorSwitch}>WEB APPLICATIONS</IntroText>
-      {projects.map((p, i) => (
-        <ProjectsContainer key={i} even={p.projectID % 2 === 0 ? true : false}>
-          <ProjectImgContainer data-aos="fade-right">
-            <ProjectImg src={p.projectImage} alt={p.projectTitle} />
-          </ProjectImgContainer>
-          <ProjectDescription
-            color={p.color}
-            color2={p.color2}
-            colorSwitch={colorSwitch}
-            even={p.projectID % 2 === 0 ? true : false}
-            data-aos="fade-left">
-            <Content>
-              <h1>{p.projectTitle}</h1>
-              <p>{p.description}</p>
-              <p>{p.language}</p>
-            </Content>
-          </ProjectDescription>
-        </ProjectsContainer>
-      ))}
-    </ProjectsWrapper>
+    <>
+      <ProjectsWrapper id="ToProject" data-aos="fade-right">
+        <IntroText colorSwitch={colorSwitch}>WEB APPLICATIONS</IntroText>
+        {projects.map((p, i) => (
+          <ProjectsContainer
+            key={i}
+            even={p.projectID % 2 === 0 ? true : false}>
+            <ProjectImgContainer data-aos="fade-right">
+              <ProjectImg
+                src={p.projectImage}
+                alt={p.projectTitle}
+                toggleModal={toggleModal}
+                modalContent={modalContent}
+                onClick={() => {
+                  dispatch(setModal(true));
+                  dispatch(setModalContent(p.projectImage));
+                }}
+              />
+            </ProjectImgContainer>
+            <ProjectDescription
+              color={p.color}
+              color2={p.color2}
+              colorSwitch={colorSwitch}
+              even={p.projectID % 2 === 0 ? true : false}
+              data-aos="fade-left">
+              <Content>
+                <h1>{p.projectTitle}</h1>
+                <p>{p.description}</p>
+                <p>{p.language}</p>
+              </Content>
+            </ProjectDescription>
+          </ProjectsContainer>
+        ))}
+      </ProjectsWrapper>
+      {toggleModal === true ? <PhotoModal /> : null}
+    </>
   );
 };
 
